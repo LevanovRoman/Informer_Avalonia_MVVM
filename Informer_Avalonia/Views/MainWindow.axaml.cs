35f96a915930084cb6b1c574e56d978df4004b8e
levanovroman <levanovroman2016@yandex.ru>
2024-01-23 14:24:49 +0300

    imap one letter

--- Informer_Avalonia/Views/MainWindow.axaml.cs
+++ Informer_Avalonia/Views/MainWindow.axaml.cs
@@ -1,6 +1,17 @@
 using System.IO;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Informer_Avalonia.ViewModels;
+
+using System;
+using System.Net;
+using System.Threading;
+
+using MailKit.Net.Imap;
+using MailKit.Search;
+using MailKit;
+using MailKit.Security;
+using MimeKit;
 
 namespace Informer_Avalonia.Views;
 
@@ -9,12 +20,13 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        DataContext = new MainWindowViewModel();
     }
     
     private void Clear_OnClick(object? sender, RoutedEventArgs e)
     {
         Clear();
-        StatusBar.Text = "Cleared!";
+        StatusBar1.Text = "Cleared!";
     }
 
     private void Clear()
@@ -30,13 +42,49 @@ public partial class MainWindow : Window
 
         if (string.IsNullOrWhiteSpace(first) || string.IsNullOrWhiteSpace(last))
         {
-            StatusBar.Text = "Warning! One of the fields is empty";
+            StatusBar1.Text = "Warning! One of the fields is empty";
             return;
         }
        
         using var file = new StreamWriter("output.dat", append: true);
         file.WriteLine($"{last} {first}");
         Clear();
-        StatusBar.Text = $"{last} ---- {first}";
+        Mail(first, last);
+        // ;
     }
+
+    private void Mail(string name, string password)
+    { 
+        using (var client = new ImapClient ())
+        {
+                try
+                {
+                    client.Connect ("cgp.nordsy.spb.ru", 143, SecureSocketOptions.StartTls);
+                    client.Authenticate (name, password);
+                    
+                    var inbox = client.Inbox;
+                    inbox.Open (FolderAccess.ReadOnly);
+
+                    Console.WriteLine ("Total messages: {0}", inbox.Count);
+                    Console.WriteLine ("Recent messages: {0}", inbox.Recent);
+
+                    for (int i = inbox.Count-1; i > inbox.Count - 4 ; i--) {
+                        var message = inbox.GetMessage (i);
+                        StatusBar1.Text = $"Отправитель: {message.From}";
+                        StatusBar2.Text = $"Тема: {message.Subject}";
+                        StatusBar3.Text = $"Дата: {message.Date}";
+                    }
+                    client.Disconnect (true);
+                }
+                catch ( MailKit.Security.AuthenticationException e)
+                {
+                    StatusBar1.Text = $"Incorrect: name: {name};  password: {password}";
+                }
+                catch ( Exception e)
+                {
+                    StatusBar1.Text = $"Incorrect: name: {name};  password: {password}; {e}";
+                }
+        }
+    }
+    
 }
--- Informer_Avalonia/obj/Debug/net8.0/Informer_Avalonia.AssemblyInfo.cs
+++ Informer_Avalonia/obj/Debug/net8.0/Informer_Avalonia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Informer_Avalonia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23771da3201163d29bf9cdfb75e8d5f48e8f661b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Informer_Avalonia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Informer_Avalonia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
